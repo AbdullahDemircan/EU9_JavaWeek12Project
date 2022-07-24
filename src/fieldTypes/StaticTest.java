15dea7287edcbffd27a8863b0628e7d3916a0247
AbdullahDemircan <abdullah.demircan.78@gmail.com>
2022-07-24 21:42:42 +0200

    commit

---
+++ src/fieldTypes/StaticTest.java
@@ -0,0 +1,10 @@
+package fieldTypes;
+
+public class StaticTest {
+    public static void main(String[] args) {
+        StaticMethods.displayMessage("Hello from static method");
+        StaticMethods object = new StaticMethods();
+        object.displayMessage("We are using object to call this static method");
+        System.out.println(StaticMethods.count);
+    }
+}
